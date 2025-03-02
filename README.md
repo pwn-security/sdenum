ae8b6092d5f0014cfbe2732c7673a956810b8e36
Purshottam Nepal <prunepal3339@gmail.com>
2025-03-02 08:28:17 +0545

    [Feature]: crt.sh search

---
+++ README.md
@@ -0,0 +1,32 @@
+# sdenum
+A subdomain enumeration tool for OSINT enthusiasts
+
+### Current Feature
+* crt.sh search ✅
+
+### Setup Guide (RECOMMENDED): [LINUX USERS]
+Install python3 and pip: **Google it**
+
+Install venv: `pip install virtualenv`
+
+Setup venv: `python3 -m venv venv`
+
+Activate the Virtual Environment:
+
+`source ./venv/bin/activate`
+
+Install dependencies:`pip install -r requirements.txt`
+
+
+### Usage Guide
+
+(venv)$ `python3 sdenum.py <DOMAIN_NAME>`
+
+
+
+### Future Improvements
+1. Additional strategies for subdomain enumeration
+* [Google Dorks](https://exploit-db.com/google-hacking-database)
+* Brute-force
+* DNS Scraping
+2. Support for multi-threading
---
+++ sdenum.py
@@ -0,0 +1,50 @@
+import sys
+import requests
+import argparse
+
+def fetch_certificates(domain):
+    url = f"https://crt.sh/?q={domain}&output=json"
+    headers = {"User-Agent": "Mozilla/5.0"}  # Set a User-Agent to avoid blocking
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an error for HTTP error codes
+        
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
+
+def main():
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Fetch subdomain for a given domain.")
+    
+    # Add arguments for the domain and verbosity flags
+    parser.add_argument("domain", help="The domain to fetch from")
+    parser.add_argument(
+        "-v", "--verbose",
+        action="count",  # Count the number of -v/-V flags
+        default=0,  # Default verbosity level is 0
+        help="Increase output verbosity (use -v for verbose, -vv for more verbose)."
+    )
+    
+    # Parse arguments
+    args = parser.parse_args()
+    domain = args.domain
+    verboseLevel = min(args.verbose, 2)  # Limit verbosity to 2
+    certificates = fetch_certificates(domain)
+
+    if certificates:
+        certificates_having_common_name = filter(lambda cert: "common_name" in cert, certificates)
+        common_names = set(map(lambda cert: cert["common_name"], certificates_having_common_name))
+        
+        if verboseLevel > 0:
+            print(f"Found {len(common_names)} subdomains in {domain}")
+        if verboseLevel > 1:
+            print(f"Subdomains: {common_names}")
+    else:
+        print("No certificates found or an error occurred.")
+
+if __name__ == "__main__":
+    main()
